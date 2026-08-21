5620b65f5ccdd7763f144753fc4a96c59f456189
asbcorp24 <69872347+asbcorp24@users.noreply.github.com>
2026-08-21 14:18:57 +0300

    Добавить типы полей библиотеки ресурсов

--- laravel9/app/Models/ResourceLibrary.php
+++ laravel9/app/Models/ResourceLibrary.php
@@ -1,1 +1,7 @@
-<?php namespace App\Models; class ResourceLibrary extends LegacyAdminModel {protected $table='resource_library';}
+<?php
+namespace App\Models;
+class ResourceLibrary extends LegacyAdminModel
+{
+    protected $table='resource_library';
+    protected $casts=['settings'=>'array','is_active'=>'boolean','dated_at'=>'date'];
+}
